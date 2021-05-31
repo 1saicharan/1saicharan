644d382a94981ae82b58cf17d2a96cf2c49e5548
saicharan <skudikala089@gmail.com>
2021-05-31 15:11:40 +0530

    practice for the familiarisation of git

---
+++ practice_newfile2.py
@@ -0,0 +1,27 @@
+
+
+import random
+c=random.randint(1,100)
+k=0
+g=0
+while g!=c:
+
+    print('guess the number until you are correct')
+    print ("so now guess the number.. ")
+    g = int(input("guess the number"))
+    if  1<=g<=100  :
+        pass
+
+    else :
+        print("OUT OF THE BOUNDS")
+        k=k+1
+        continue
+    if g<c:
+        print("warmer")
+        k=k+1
+        continue
+    elif g>c:
+        print ("colder")
+        k=k+1
+        continue
+print("you gussed it correctly and you gussed it by attempting {} times ".format(k+1))
